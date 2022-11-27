c7a369a11d33af7c0de0e0ed87a62e0bbb6546c9
mercury6699 <bibaryskoutepov@mail.ru>
2022-11-27 19:05:23 +0600

    posts.create and posts.store (Without validation)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,29 +4,43 @@ namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
         return view("posts.index",["posts" => BlogPost::all()]);
     }
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+
+//    public function show($id) There are another way how to write this method
+//    {
+//        //
+//        return view('posts.show', ["post" => BlogPost::findOrFail($id)]);
+//    }
+
+    public function show(Request $request, BlogPost $post)
     {
         //
-        return view('posts.show', ["post" => BlogPost::findOrFail($id)]);
+//        $request->session()->reflash();
+        return view('posts.show', ["post" => $post]);
     }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $blogPost = new BlogPost();
+        $blogPost->title = $request->input('title');
+        $blogPost->content = $request->input('content');
+        $blogPost->save();
+
+        $request->session()->flash('status','Blog post was created successfully!');
+
+        return redirect()->route('posts.show', ['post' => $blogPost->id]);
+    }
+
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,8 +12,16 @@
     <li><a href="{{ route('home') }}">Home</a></li>
     <li><a href="{{ route('contact') }}">Contact</a></li>
     <li><a href="{{ route('posts.index') }}">Blog Posts</a></li>
+    <li><a href="{{ route('posts.create') }}">Add Blog Post</a></li>
 
 </ul>
+
+@if(session()->has("status"))
+    <p style="color: green">
+        {{ session()->get("status") }}
+    </p>
+@endif
 @yield("content")
+
 </body>
 </html>
