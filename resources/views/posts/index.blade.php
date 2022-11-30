db0ae1115681d63a96ce4b0200eac788b126b094
mercury6699 <bibaryskoutepov@mail.ru>
2022-11-30 13:03:59 +0600

    Author and Profile models were created, One to One relation

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,24 +2,26 @@
 
 @section("content")
 @forelse ($posts as $post)
-    <p>
-        <h3>
-            <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
-        </h3>
+    <div class="container">
+        <p>
+            <h3>
+                <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
+            </h3>
 
-        <div class="row">
-            <div class="col-md-1">
-                <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-primary btn-block" >Edit</a>
+            <div class="row">
+                <div class="col-md-1">
+                    <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-primary btn-block" >Edit</a>
+                </div>
+                <div class="col-md-0">
+                    <form method="POST" class="form-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Delete!" class="btn btn-danger btn-block"/>
+                    </form>
+                </div>
             </div>
-            <div class="col-md-0">
-                <form method="POST" class="form-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}">
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" value="Delete!" class="btn btn-danger btn-block"/>
-                </form>
-            </div>
-        </div>
-    </p>
+        </p>
+    </div>
 @empty
     <p>No blog posts yet!</p>
 @endforelse
