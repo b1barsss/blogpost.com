6bcb4c9622668d5790d00c5c6db38bde69a8f11e
mercury6699 <bibaryskoutepov@mail.ru>
2022-12-01 23:15:22 +0600

    auth.register.blade.php was created successfully

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,47 @@
+@extends('layout')
+@section('content')
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <div class='form-group'>
+            <label for="">Name:</label>
+            <input name="name" value="{{ old('name') }}" placeholder="Username" required
+                   class="form-control {{ $errors->has('name') ? ' is-invalid' : '' }}">
+            @if($errors->has('name'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label for="">E-mail:</label>
+            <input name="email" value="{{ old('email') }}" placeholder="example@gmail.com" required
+                   class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" >
+            @if($errors->has('email'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label for="">Password:</label>
+            <input name="password" placeholder="************" type="password" required
+                   class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}">
+            @if($errors->has('password'))
+                <span class="invalid-feedback">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label for="">Retype password:</label>
+            <input name="password_confirmation" type="password" class="form-control " placeholder="************" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary btn-block">Register!</button>
+    </form>
+@endsection
+
